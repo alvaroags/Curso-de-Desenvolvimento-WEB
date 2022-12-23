87afa8a616e64382d0cf822c15c36ab9c259f8d2
alvaroags <alvarogsilva.neto@gmail.com>
2022-12-23 13:18:41 -0300

    Create ex001.php

---
+++ Projetos PHP/ex001.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Curso de PHP</title>
+    </head>
+    <body>
+        <?php 
+            echo 'Utilizando a tag padrão'
+        ?>
+        </br>
+        <?= 'Utilizado a tag de impressão' ?> 
+        </br>
+        </br>
+        <?php
+            echo 'Utilizando o echo<br>';
+            print "Utilizando o print<hr>";
+        ?>
+    </body>
+</html>
