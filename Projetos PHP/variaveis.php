a8233a8bfd6c35444299afdddc868326375cb41c
alvaroags <alvarogsilva.neto@gmail.com>
2022-12-24 11:37:37 -0300

    Create variaveis.php

---
+++ Projetos PHP/variaveis.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Variaveis PHP</title>
+    </head>
+    <body>
+        <?php 
+            //strings
+            $nome = "Alvaro Gomes";
+            //int
+            $idade = 19;
+            //float 
+            $peso = 62.6;
+            //booleana
+            $gamer = true;
+        ?>
+        
+        <h1>Ficha cadastral</h1>
+        <p>Nome: <?php echo $nome ?> </p>
+        <p>Idade: <?php echo $idade ?> </p>
+        <p>Peso: <?php echo $peso ?> </p>
+        <p>Gamer: <?php echo $gamer ?> </p>
+    </body>
+</html>
