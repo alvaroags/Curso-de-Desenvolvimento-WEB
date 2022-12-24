dffb8366e64d4783c3c616eb714d7ff7b1391720
alvaroags <alvarogsilva.neto@gmail.com>
2022-12-24 18:04:36 -0300

    Create manipulacao_matematica.php

---
+++ Projetos PHP/manipulacao_matematica.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Manipulação Matematicas</title>
+    </head>
+    <body>
+        <?php
+        $numero = 14.5;
+        echo ceil($numero) .'</br>';
+        echo floor($numero) .'</br>';
+        echo round($numero) .'</br>';
+        echo rand(0, 124) .'</br>';
+        echo sqrt($numero);
+        ?>
+    </body>
+</html>
