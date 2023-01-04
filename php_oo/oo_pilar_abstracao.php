2cd4db67c070aa061904b327f2370573b09306e6
alvaroags <alvarogsilva.neto@gmail.com>
2023-01-04 17:54:49 -0300

    Update oo_pilar_abstracao.php

--- php_oo/oo_pilar_abstracao.php
+++ php_oo/oo_pilar_abstracao.php
@@ -2,9 +2,36 @@
 
     //modelo
 class Funcionario{
-    public $nome = "Alvaro Gomes";
-    public $telefone = "(37)99999-9999";
-    public $numFilhos = 0;
+    public $nome = null;
+    public $telefone = null;
+    public $numFilhos = null;
+    public $cargo = null;
+    public $salario = null;
+
+    function __set($atributo, $valor){
+        $this->$atributo = $valor;
+    }
+    function __get($atributo){
+        return $this->$atributo;
+    }
+    // function setNome($nome){
+    //     $this->nome = $nome;
+    // }
+    // function setTelefone($telefone){
+    //     $this->telefone = $telefone;
+    // }
+    // function setNumFilhos($numFilhos){
+    //     $this->numFilhos = $numFilhos;
+    // }
+    // function getNome(){
+    //     return $this->nome;
+    // }
+    // function getTelefone(){
+    //     return $this->telefone;
+    // }
+    // function getNumFilhos(){
+    //     return $this->numFilhos;
+    // }
     function resumirCadFunc(){
         return "$this->nome possui $this->numFilhos filho(s)";
     }
@@ -14,8 +41,12 @@ class Funcionario{
 }
 //metodos
 $funcionario = new Funcionario();
-echo $funcionario->resumirCadFunc();
-$funcionario->modificarNumFilhos(3);
-echo '<br>';
-echo $funcionario->resumirCadFunc();
+$funcionario->__set("nome","Álvaro");
+$funcionario->__set("numFilhos", 2);
+echo $funcionario->__get('nome') . ' possui ' . $funcionario->__get('numFilhos') . ' filho(s)';
+echo '<hr>';
+$funcionario2 = new Funcionario();
+$funcionario2->__set("nome","Joao");
+$funcionario2->__set("numFilhos", 4);
+echo $funcionario2->__get('nome') . ' possui ' . $funcionario2->__get('numFilhos') . ' filho(s)';
 ?>
