66680a46fded4f157650e67c3c1e1aec82ba8c5a
alvaroags <alvarogsilva.neto@gmail.com>
2023-01-05 11:49:43 -0300

    Update oo_pilar_heranca.php

--- php_oo/oo_pilar_heranca.php
+++ php_oo/oo_pilar_heranca.php
@@ -15,6 +15,8 @@ class Moto extends Veiculo{
         echo 'Empinar';
     }
 }
+//Como carro e moto são veiculos eles tem coisas em comum, como uma forma de reduzir a produção de codigo é possivel usar a herança para atribuir uma classe dentro de outra
+//Para fazer a conexão basta na classe filho usar o comando 'extends' e o nome da classe pai e todas as variaveis e funçãos da classe pai estará disponivel na filho
 class Veiculo{
     public $cor = null;
     public $placa = null;
@@ -26,6 +28,7 @@ class Veiculo{
         $this->placa = $placa;
     }
 }
+//Usando a função construct da classe pai para instanciar declarando novos valores nas classes filhos
 $carro = new Carro('Abc1234', 'Branco');
 $moto = new Moto('jhu6542', 'Preto');
 echo $carro->acelerar();
